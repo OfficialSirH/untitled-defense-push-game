34603d7c2f6c9d04111670b063eb2eb1b20375cf
megatank58 <tanumegatank@gmail.com>
2025-07-16 13:46:14 +0530

    fix: errors

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,16 +1,16 @@
 extends Control
 
-var game = preload("res://scenes/game.tscn")
+var level1 = preload("res://scenes/level1.tscn")
 
 func _ready() -> void:
 	if OS.has_feature("dedicated_server"):
-		GameManager.host_multiplayer(game)
+		GameManager.host_multiplayer(level1)
 
 func _play_game() -> void:
-	GameManager.play_singleplayer(game)
+	GameManager.play_singleplayer(level1)
 
 func _host_game() -> void:
-	GameManager.host_multiplayer(game)
+	GameManager.host_multiplayer(level1)
 
 func _join_game() -> void:
-	GameManager.join_multiplayer(game)
+	GameManager.join_multiplayer(level1)
