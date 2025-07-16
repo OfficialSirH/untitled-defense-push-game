64603ec00f10f6ab6491a72d6f1cb62b3076aa18
megatank58 <tanumegatank@gmail.com>
2025-07-16 14:54:33 +0530

    feat: coin (#2)

---
+++ scripts/coin.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+func _ready() -> void:
+	$"../HUD/ScoreLabel".text = str(GameManager.score)
+
+func _on_body_entered(body: Node2D) -> void:
+	GameManager.score += 1
+	$"../HUD/ScoreLabel".text = str(GameManager.score)
+	queue_free()
